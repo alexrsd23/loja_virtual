a34e7bf8a554adee6c9e0020a3bb1d79646f25e9
alexrsd23 <alexrosendo23@outlook.com>
2024-06-11 09:26:03 -0300

    Criacao dos testes de delecao

--- loja_virtual/src/main/java/com/rosendo/loja_virtual/domain/usuario/CadastroUsuarioDTO.java
+++ loja_virtual/src/main/java/com/rosendo/loja_virtual/domain/usuario/CadastroUsuarioDTO.java
@@ -1,6 +1,7 @@
 package com.rosendo.loja_virtual.domain.usuario;
 
 import com.rosendo.loja_virtual.config.role.Role;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -10,7 +11,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CadastroUsuarioDTO {
-    @NotBlank(message = "email é necessário.")
+    @NotBlank(message = "O email é obrigatório")
+    @Email(message = "Email inválido")
     private String email;
     @NotBlank(message = "login é necessário.")
     @Size(min = 4, max = 10, message="É necessário ter entre 4 e 10 caracteres.")
